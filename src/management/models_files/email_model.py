03da0f8a1b21f59268e94cf10ef50746bed6388f
SzymKam <szymon15kaminski@gmail.com>
2023-11-04 21:53:06 +0100

    fix: add models files

---
+++ src/management/models_files/email_model.py
@@ -0,0 +1,16 @@
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
+from django.utils import timezone
+
+from .mailbox_model import Mailbox
+from .template_model import Template
+
+
+class Email(models.Model):
+    mailbox = models.ForeignKey(Mailbox, on_delete=models.CASCADE, related_name="email")
+    template = models.ForeignKey(Template, on_delete=models.SET_NULL, related_name="email")
+    to = ArrayField(base_field=models.EmailField)
+    cc = ArrayField(base_field=models.EmailField)
+    reply_to = models.EmailField(default=None, blank=True, null=True)
+    send_date = models.DateTimeField(blank=True, null=True, default=None)
+    date = models.DateTimeField(default=timezone.now)
---
+++ src/management/models_files/mailbox_model.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Mailbox(models.Model):
+    host = models.CharField()
+    port = models.IntegerField(default=465)
+    login = models.CharField()
+    password = models.CharField()
+    email_from = models.EmailField()
+    use_ssl = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False, null=True, blank=True)
+    date = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    last_update = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    sent = models.IntegerField(null=True, blank=True)
