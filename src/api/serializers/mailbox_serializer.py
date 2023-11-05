02d2322668f1e1859ae5523047fb53fec0053b2c
SzymKam <szymon15kaminski@gmail.com>
2023-11-05 17:04:48 +0100

    fix: update serializer - add validate login and emial - just for one user

--- src/api/serializers/mailbox_serializer.py
+++ src/api/serializers/mailbox_serializer.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 from management.models import Mailbox
 
@@ -7,3 +7,14 @@ class MailboxSerializer(ModelSerializer):
     class Meta:
         model = Mailbox
         fields = "__all__"
+
+    def validate(self, data):
+        email = data.get("email_from")
+        if Mailbox.objects.filter(email_from=email).exists():
+            raise ValidationError("Email is already in use.")
+
+        login = data.get("login")
+        if Mailbox.objects.filter(login=login).exists():
+            raise ValidationError("Login is already in use.")
+
+        return data
