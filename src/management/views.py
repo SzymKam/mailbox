37547419994d99274d55542dae00a83675256493
SzymKam <szymon15kaminski@gmail.com>
2023-11-07 18:49:21 +0100

    feat: add email sending view

--- src/management/views.py
+++ src/management/views.py
@@ -1,3 +1,11 @@
+from django.core.mail import send_mail
 from django.shortcuts import render
 
-# Create your views here.
+
+def email_sending():
+    send_mail(
+        subject="Szymon",
+        from_email="szymon15kaminski@gmail.com",
+        message="koza",
+        recipient_list=["szymon15kaminski@gmail.com"],
+    )
