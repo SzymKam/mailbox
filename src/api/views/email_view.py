fbc770a29854e1023abba659bba9f50eea1526ef
SzymKam <szymon15kaminski@gmail.com>
2023-11-07 20:55:48 +0100

    fix: start email_management class when email create

--- src/api/views/email_view.py
+++ src/api/views/email_view.py
@@ -3,8 +3,12 @@ from rest_framework.viewsets import GenericViewSet
 
 from api.serializers.email_serializer import EmailSerializer
 from management.models import Email
+from management.views import EmailManagement
 
 
 class EmailView(GenericViewSet, CreateModelMixin, ListModelMixin):
     queryset = Email.objects.all()
     serializer_class = EmailSerializer
+
+    def perform_create(self, serializer):
+        email_management = EmailManagement(data=serializer.data)
