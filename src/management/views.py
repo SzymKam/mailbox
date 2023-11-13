b003b962ac5d2992c910f91d541ce054d755c92d
SzymKam <szymon15kaminski@gmail.com>
2023-11-13 23:41:38 +0100

    fix: update email message args

--- src/management/views.py
+++ src/management/views.py
@@ -23,13 +23,13 @@ class EmailManagement:
         email = EmailMessage(
             subject=self.template.subject,
             body=self.template.text,
-            from_email="szymon15kaminski@gmail.com",
-            to=["szymon15kaminski@gmail.com"],
+            from_email=self.mailbox.email_from,
+            to=self.to,
             # not obligatory values
             cc=self.cc,
             bcc=self.bcc,
-            reply_to=self.reply_to
-            # attachments=self.template.attachment,
+            reply_to=self.reply_to,
+            attachments=self.template.attachment,
         )
         email.send()
 
