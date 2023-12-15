4622a9b7d3ec2ad6bc5226c392b4642574abd36f
SzymKam <szymon15kaminski@gmail.com>
2023-12-15 23:34:11 +0300

    fix: add defaut debug task into celery

--- src/core/celery.py
+++ src/core/celery.py
@@ -9,3 +9,8 @@ app = Celery("core")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+#
+# @app.task(bind=True, ignore_result=True)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
