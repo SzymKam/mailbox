d7d2f6cd25dee9f2dad0ddb7c2e41d98e718f632
SzymKam <szymon15kaminski@gmail.com>
2023-12-20 20:58:53 +0100

    fix: overwrite create method instead of perform create

--- src/api/views/email_view.py
+++ src/api/views/email_view.py
@@ -3,12 +3,18 @@ from rest_framework.viewsets import GenericViewSet
 
 from api.serializers.email_serializer import EmailSerializer
 from management.models import Email
-from management.views import EmailManagement
+from management.views.email_management_view import EmailManagement
 
 
 class EmailView(GenericViewSet, CreateModelMixin, ListModelMixin):
     queryset = Email.objects.all()
     serializer_class = EmailSerializer
 
-    def perform_create(self, serializer):
-        email_management = EmailManagement(data=serializer.data)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        email_management = EmailManagement(data=serializer.data, headers=headers)
+        email_management.send()
