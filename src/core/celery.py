b71e855825e08dca8b2db060a0f70be947497018
SzymKam <szymon15kaminski@gmail.com>
2023-12-28 22:49:12 +0100

    fix: update celery config file

--- src/core/celery.py
+++ src/core/celery.py
@@ -1,12 +1,17 @@
 import os
 
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 app = Celery("core")
 
-app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.enable_utc = False
+
+app.conf.update(timezone="Europe/Berlin")
+
+app.config_from_object(settings, namespace="CELERY")
 
 app.autodiscover_tasks()
 
